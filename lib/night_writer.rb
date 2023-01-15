4725d72a4d019a9690cbc035127e475c52d30745
James Taylor <jrtaylor0614@gmail.com>
2023-01-14 18:58:44 -0700

    Refactor: night_writer to runner

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,6 +1,15 @@
-def Night_writer
+input = ARGV[0]
 
-    def initialize
+output = ARGV[1]
 
-    end
-end
+handle = File.open(ARGV[0], "r")
+incoming = handle.read
+handle.close
+
+
+writer = File.open(ARGV[1], "w")
+
+writer.close
+
+
+p "Created #{input} containing #{incoming.size} characters"
