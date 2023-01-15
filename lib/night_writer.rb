286aebbbeac8304b3c11717dc4e696a1a95f25e1
James Taylor <jrtaylor0614@gmail.com>
2023-01-15 13:29:14 -0700

    Feat: #english_to_braille

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,13 +1,13 @@
-input = ARGV[0]
-output = ARGV[1]
+# input = ARGV[0]
+# output = ARGV[1]
 
-reader = File.open(ARGV[0], 'r')
-text = reader.read
-reader.close
+# reader = File.open(ARGV[0], 'r')
+# text = reader.read
+# reader.close
 
 
-writer = File.open(ARGV[1], "w")
-text_2 = writer.write(text.upcase) 
-writer.close
+# writer = File.open(ARGV[1], "w")
+# text_2 = writer.write(text.upcase) 
+# writer.close
 
-p "Created #{output} containing #{text_2} characters"
+# p "Created #{output} containing #{text_2} characters"
